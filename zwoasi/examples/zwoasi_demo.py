ca5249ff7179977ba0f64bbb945f555d63609c2e
Steve Marple <stevemarple@googlemail.com>
2016-12-27 14:30:45 +0000

    Update demo

--- zwoasi/examples/zwoasi_demo.py
+++ zwoasi/examples/zwoasi_demo.py
@@ -5,36 +5,49 @@ import sys
 import zwoasi as asi
 
 num_cameras = asi.get_num_cameras()
-print('Num cameras: ' + str(num_cameras))
 if num_cameras == 0:
     print('No cameras found')
     sys.exit(0)
 
-asi.list_cameras()
-
 cam = asi.Camera(0)
-print(cam)
-print(cam.get_roi_format())
-print(cam.get_roi_start_position())
-print(cam.get_control_value(asi.ASI_TEMPERATURE))
+cam_info = cam.get_camera_property()
+
+if num_cameras == 1:
+    print('Found one camera: %s' % cam_info['Name'])
+else:
+    print('Found %d cameras, using #0' % num_cameras)
+    print('\n'.join(asi.list_cameras()))
+
+# Set some sensible defaults. They will need adjusting depending upon
+# the sensitiy, lens and lighting conditions used.
 
 cam.set_control_value(asi.ASI_GAIN, 150)
-cam.set_control_value(asi.ASI_EXPOSURE, 50000)
+cam.set_control_value(asi.ASI_EXPOSURE, 30000)
 cam.set_control_value(asi.ASI_WB_B, 99)
 cam.set_control_value(asi.ASI_WB_R, 75)
 cam.set_control_value(asi.ASI_GAMMA, 50)
 cam.set_control_value(asi.ASI_BRIGHTNESS, 50)
 cam.set_control_value(asi.ASI_FLIP, 0)
 
-print('===')
-img = cam.acquire()
-print('---')
-print(len(img))
+print('Acquiring a single 8-bit mono image')
+filename = 'image_mono.png'
+cam.set_image_type(asi.ASI_IMG_RAW8)
+img = cam.acquire(filename=filename)
+print('Saved to %s' % filename)
 
-cam.set_image_type(asi.ASI_IMG_RGB24)
 
-print('===')
-img = cam.acquire()
-print('---')
-print(len(img))
-print(cam.get_roi_format())
+print('Acquiring a single 16-bit mono image')
+filename = 'image_mono16.png'
+cam.set_image_type(asi.ASI_IMG_RAW16)
+cam.acquire(filename=filename)
+print('Saved to %s' % filename)
+
+if cam_info['IsColorCam']:
+    filename = 'image_color.png'
+    cam.set_image_type(asi.ASI_IMG_RGB24)
+    print('Acquiring a single, color image')
+    cam.acquire(filename=filename)
+    print('Saved to %s' % filename)
+else:
+    print('Color image not available with this camera')
+    
