6488fcb89cbbc24dd55cef91abd84df3e1934a35
Steve Marple <stevemarple@googlemail.com>
2016-12-28 22:19:46 +0000

    Add missing functions and reorder
    Cannot add call to ASISetID() - uncertainty over correct parameters.

--- zwoasi/__init__.py
+++ zwoasi/__init__.py
@@ -34,19 +34,19 @@ def _open_camera(id):
     return
 
 
-def _close_camera(id):
-    r = zwolib.ASICloseCamera(id)
-    if r:
-        raise zwo_errors[r]
-    return
-
-
 def _init_camera(id):
     r = zwolib.ASIInitCamera(id)
     if r:
         raise zwo_errors[r]
     return
    
+
+def _close_camera(id):
+    r = zwolib.ASICloseCamera(id)
+    if r:
+        raise zwo_errors[r]
+    return
+
 
 def _get_num_controls(id):
     num = c.c_int()
@@ -60,8 +60,24 @@ def _get_control_caps(id, control_index):
     caps = _ASI_CONTROL_CAPS()
     r = zwolib.ASIGetControlCaps(id, control_index, caps)
     if r:
-        raise zwo_errors(r)
+        raise zwo_errors[r]
     return caps.get_dict()
+
+
+def _get_control_value(id, control_type):
+    value = c.c_long()
+    auto = c.c_int()
+    r = zwolib.ASIGetControlValue(id, control_type, value, auto)
+    if r:
+        raise zwo_errors[r]
+    return [value.value, bool(auto.value)]
+
+
+def _set_control_value(id, control_type, value, auto):
+    r = zwolib.ASISetControlValue(id, control_type, value, auto)
+    if r:
+        raise zwo_errors[r]
+    return
 
 
 def _get_roi_format(id):
@@ -71,7 +87,7 @@ def _get_roi_format(id):
     image_type = c.c_int()
     r = zwolib.ASIGetROIFormat(id, roi_width, roi_height, bins, image_type)
     if r:
-        raise zwo_errors(r)
+        raise zwo_errors[r]
     return [roi_width.value, roi_height.value, bins.value, image_type.value]
 
 
@@ -98,7 +114,7 @@ def _set_roi_format(id, width, height, bins, image_type):
                          cam_info['Name'])
     r = zwolib.ASISetROIFormat(id, width, height, bins, image_type)
     if r:
-        raise zwo_errors(r)
+        raise zwo_errors[r]
     return
 
 
@@ -107,7 +123,7 @@ def _get_start_position(id):
     start_y = c.c_int()
     r = zwolib.ASIGetStartPos(id, start_x, start_y)
     if r:
-        raise zwo_errors(r)
+        raise zwo_errors[r]
     return [start_x.value, start_y.value]
 
 
@@ -119,81 +135,43 @@ def _set_start_position(id, start_x, start_y):
 
     r = zwolib.ASISetStartPos(id, start_x, start_y)
     if r:
-        raise zwo_errors(r)
+        raise zwo_errors[r]
     return
 
 
-def _get_control_value(id, control_type):
-    value = c.c_long()
-    auto = c.c_int()
-    r = zwolib.ASIGetControlValue(id, control_type, value, auto)
+def _get_dropped_frames(id):
+    dropped_frames = c.c_int()
+    r = zwo_errors.ASIGetDroppedFrames(id, dropped_frames)
     if r:
-        raise zwo_errors(r)
-    return [value.value, bool(auto.value)]
+        raise zwo_errors[r]
+    return dropped_frames.value
 
-
-def _set_control_value(id, control_type, value, auto):
-    r = zwolib.ASISetControlValue(id, control_type, value, auto)
-    if r:
-        raise zwo_errors(r)
-    return
-
-
-def _start_exposure(id, is_dark):
-    r = zwolib.ASIStartExposure(id, is_dark)
-    if r:
-        raise zwo_errors(r)
-    return
-
-
-def _stop_exposure(id):
-    r = zwolib.ASIStopExposure(id)
-    if r:
-        raise zwo_errors(r)
-    return
-
-
-def _get_exposure_status(id):
-    status = c.c_int()
-    r = zwolib.ASIGetExpStatus(id, status)
-    if r:
-        raise zwo_errors(r)
-    return status.value
-
-
-def _get_data_after_exposure(id, buffer=None):
-    if buffer is None:
-        whbi = _get_roi_format(id)
-        sz = whbi[0] * whbi[1]
-        if whbi[3] == ASI_IMG_RGB24:
-            sz *= 3
-        elif whbi[3] == ASI_IMG_RAW16:
-            sz *= 2
-        buffer = bytearray(sz)
-    else:
-        if not isinstance(buffer, bytearray):
-            raise TypeError('supplied buffer must be a bytearray')
-        sz = len(buffer)
     
-    cbuf_type = c.c_char * len(buffer)
-    cbuf = cbuf_type.from_buffer(buffer)
-    r = zwolib.ASIGetDataAfterExp(id, cbuf, sz)
-    
+def _enable_dark_subtract(id, filename):
+    r = zwolib.ASIEnableDarkSubtract(id, filename)
     if r:
-        raise zwo_errors(r)
-    return buffer
+        raise zwo_errors[r]
+    return
+
+
+def _disable_dark_subtract(id):
+    r = zwolib.ASIDisableDarkSubtract(id)
+    if r:
+        raise zwo_errors[r]
+    return
+    
 
 def _start_video_capture(id):
     r = zwolib.ASIStartVideoCapture(id)
     if r:
-        raise zwo_errors(r)
+        raise zwo_errors[r]
     return
     
 
 def _stop_video_capture(id):
     r = zwolib.ASIStopVideoCapture(id)
     if r:
-        raise zwo_errors(r)
+        raise zwo_errors[r]
     return
 
 
@@ -216,23 +194,95 @@ def _get_video_data(id, timeout, buffer=None):
     r = zwolib.ASIGetVideoData(id, cbuf, sz, timeout)
     
     if r:
-        raise zwo_errors(r)
+        raise zwo_errors[r]
     return buffer
 
 
-def _enable_dark_subtract(id, filename):
-    r = zwolib.ASIEnableDarkSubtract(id, filename)
+def _pulse_guide_on(id, direction):
+    r = zwolib.ASIPulseGuideOn(id, direction)
     if r:
-        raise zwo_errors(r)
+        raise zwo_errors[r]
     return
 
 
-def _disable_dark_subtract(id):
-    r = zwolib.ASIDisableDarkSubtract(id)
+def _pulse_guide_off(id, direction):
+    r = zwolib.ASIPulseGuideOff(id, direction)
     if r:
-        raise zwo_errors(r)
+        raise zwo_errors[r]
     return
+
+
+def _start_exposure(id, is_dark):
+    r = zwolib.ASIStartExposure(id, is_dark)
+    if r:
+        raise zwo_errors[r]
+    return
+
+
+def _stop_exposure(id):
+    r = zwolib.ASIStopExposure(id)
+    if r:
+        raise zwo_errors[r]
+    return
+
+
+def _get_exposure_status(id):
+    status = c.c_int()
+    r = zwolib.ASIGetExpStatus(id, status)
+    if r:
+        raise zwo_errors[r]
+    return status.value
+
+
+def _get_data_after_exposure(id, buffer=None):
+    if buffer is None:
+        whbi = _get_roi_format(id)
+        sz = whbi[0] * whbi[1]
+        if whbi[3] == ASI_IMG_RGB24:
+            sz *= 3
+        elif whbi[3] == ASI_IMG_RAW16:
+            sz *= 2
+        buffer = bytearray(sz)
+    else:
+        if not isinstance(buffer, bytearray):
+            raise TypeError('supplied buffer must be a bytearray')
+        sz = len(buffer)
     
+    cbuf_type = c.c_char * len(buffer)
+    cbuf = cbuf_type.from_buffer(buffer)
+    r = zwolib.ASIGetDataAfterExp(id, cbuf, sz)
+    
+    if r:
+        raise zwo_errors[r]
+    return buffer
+
+
+def _get_id(id):
+    buffer = bytearray(8)
+    cbuf_type = c.c_char * len(buffer)
+    cbuf = cbuf_type.from_buffer(buffer)
+    r = zwolib.ASIGetID(id, cbuf)
+    if r:
+        raise zwo_errors[r]
+    return str.decode(buffer)
+
+# TO DO: need to confirm correct function call parameters
+#def _set_id(id, id_str):
+#     pass
+
+
+def _get_gain_offset(id):
+    offset_highest_DR = c.c_int()
+    offset_unity_gain = c.c_int()
+    gain_lowest_RN = c.c_int()
+    offset_lowest_RN = c.c_int()
+    r = zwolib.ASIGetGainOffset(id, offset_highest_DR, offset_unity_gain,
+                                gain_lowest_RN, offset_lowest_RN)
+    if r:
+        raise zwo_errors[r]
+    return [offset_highest_DR.value, offset_unity_gain.value,
+            gain_lowest_RN.value, offset_lowest_RN.value]
+
 
 def list_cameras():
     r = []
@@ -292,12 +342,9 @@ class Camera(object):
     def set_roi_start_position(self, start_x, start_y):
         _set_start_position(self.id, start_x, start_y)
 
-    def enable_dark_subtract(self, filename):
-        _enable_dark_subtract(self.id, filename)
-
-    def disable_dark_subtract(self):
-        _disable_dark_subtract(self.id)
-        
+    def get_dropped_frames(self):
+        return _get_dropped_frames(self.id)
+         
     def close(self):
         try:
             _close_camera(self.id)
@@ -360,6 +407,12 @@ class Camera(object):
     def get_data_after_exposure(self, buffer=None):
         return _get_data_after_exposure(self.id, buffer)
 
+    def enable_dark_subtract(self, filename):
+        _enable_dark_subtract(self.id, filename)
+
+    def disable_dark_subtract(self):
+        _disable_dark_subtract(self.id)
+        
     def start_video_capture(self):
         return _start_video_capture(self.id)
     
@@ -370,6 +423,17 @@ class Camera(object):
         if timeout is None:
             timeout = self.default_timeout
         return _get_video_data(self.id, timeout, buffer)
+
+    def pulse_guide_on(self, direction):
+        _pulse_guide_on(self.id, direction)
+        return
+    
+    def pulse_guide_off(self, direction):
+        _pulse_guide_off(self.id, direction)
+        return
+
+    def get_id(self):
+        return _get_id(self.id)
     
     # Helper functions
     def get_image_type(self):
@@ -503,6 +567,7 @@ class _ASI_CONTROL_CAPS(c.Structure):
             r[k] = bool(getattr(self, k))
         return r
 
+
 logger = logging.getLogger(__name__)
 
 # ASI_BAYER_PATTERN
@@ -584,11 +649,11 @@ zwolib.ASIGetCameraProperty.restype = c.c_int
 zwolib.ASIOpenCamera.argtypes = [c.c_int]
 zwolib.ASIOpenCamera.restype = c.c_int
 
-zwolib.ASICloseCamera.argtypes = [c.c_int]
-zwolib.ASICloseCamera.restype = c.c_int
-
 zwolib.ASIInitCamera.argtypes = [c.c_int]
 zwolib.ASIInitCamera.restype = c.c_int
+
+zwolib.ASICloseCamera.argtypes = [c.c_int]
+zwolib.ASICloseCamera.restype = c.c_int
 
 zwolib.ASIGetNumOfControls.argtypes = [c.c_int, c.POINTER(c.c_int)]
 zwolib.ASIGetNumOfControls.restype = c.c_int
@@ -596,6 +661,15 @@ zwolib.ASIGetNumOfControls.restype = c.c_int
 zwolib.ASIGetControlCaps.argtypes = [c.c_int, c.c_int,
                                      c.POINTER(_ASI_CONTROL_CAPS)]
 zwolib.ASIGetControlCaps.restype = c.c_int
+
+zwolib.ASIGetControlValue.argtypes = [c.c_int,
+                                      c.c_int,
+                                      c.POINTER(c.c_long),
+                                      c.POINTER(c.c_int)]
+zwolib.ASIGetControlValue.restype = c.c_int
+
+zwolib.ASISetControlValue.argtypes = [c.c_int, c.c_int, c.c_long, c.c_int]
+zwolib.ASISetControlValue.restype = c.c_int
 
 zwolib.ASIGetROIFormat.argtypes = [c.c_int,
                                    c.POINTER(c.c_int),
@@ -615,26 +689,14 @@ zwolib.ASIGetStartPos.restype = c.c_int
 zwolib.ASISetStartPos.argtypes = [c.c_int, c.c_int, c.c_int]
 zwolib.ASISetStartPos.restype = c.c_int
 
-zwolib.ASIGetControlValue.argtypes = [c.c_int,
-                                      c.c_int,
-                                      c.POINTER(c.c_long),
-                                      c.POINTER(c.c_int)]
-zwolib.ASIGetControlValue.restype = c.c_int
+zwolib.ASIGetDroppedFrames.argtypes = [c.c_int, c.POINTER(c.c_int)]
+zwolib.ASIGetDroppedFrames.restype = c.c_int
 
-zwolib.ASISetControlValue.argtypes = [c.c_int, c.c_int, c.c_long, c.c_int]
-zwolib.ASISetControlValue.restype = c.c_int
+zwolib.ASIEnableDarkSubtract.argtypes = [c.c_int, c.POINTER(c.c_char)]
+zwolib.ASIEnableDarkSubtract.restype = c.c_int
 
-zwolib.ASIStartExposure.argtypes = [c.c_int, c.c_int]
-zwolib.ASIStartExposure.restype = c.c_int
-
-zwolib.ASIStopExposure.argtypes = [c.c_int]
-zwolib.ASIStopExposure.restype = c.c_int
-
-zwolib.ASIGetExpStatus.argtypes = [c.c_int, c.POINTER(c.c_int)]
-zwolib.ASIGetExpStatus.restype = c.c_int
-
-zwolib.ASIGetDataAfterExp.argtypes = [c.c_int, c.POINTER(c.c_char), c.c_long]
-zwolib.ASIGetDataAfterExp.restype = c.c_int
+zwolib.ASIDisableDarkSubtract.argtypes = [c.c_int]
+zwolib.ASIDisableDarkSubtract.restype = c.c_int
 
 zwolib.ASIStartVideoCapture.argtypes = [c.c_int]
 zwolib.ASIStartVideoCapture.restype = c.c_int
@@ -648,9 +710,41 @@ zwolib.ASIGetVideoData.argtypes = [c.c_int,
                                    c.c_int]
 zwolib.ASIGetVideoData.restype = c.c_int
 
-zwolib.ASIEnableDarkSubtract.argtypes = [c.c_int, c.POINTER(c.c_char)]
-zwolib.ASIEnableDarkSubtract.restype = c.c_int
+zwolib.ASIPulseGuideOn.argtypes = [c.c_int, c.c_int]
+zwolib.ASIPulseGuideOn.restype = c.c_int
 
-zwolib.ASIDisableDarkSubtract.argtypes = [c.c_int]
-zwolib.ASIDisableDarkSubtract.restype = c.c_int
+zwolib.ASIPulseGuideOff.argtypes = [c.c_int, c.c_int]
+zwolib.ASIPulseGuideOff.restype = c.c_int
 
+zwolib.ASIStartExposure.argtypes = [c.c_int, c.c_int]
+zwolib.ASIStartExposure.restype = c.c_int
+
+zwolib.ASIStopExposure.argtypes = [c.c_int]
+zwolib.ASIStopExposure.restype = c.c_int
+
+zwolib.ASIGetExpStatus.argtypes = [c.c_int, c.POINTER(c.c_int)]
+zwolib.ASIGetExpStatus.restype = c.c_int
+
+zwolib.ASIGetDataAfterExp.argtypes = [c.c_int, c.POINTER(c.c_char), c.c_long]
+zwolib.ASIGetDataAfterExp.restype = c.c_int
+
+zwolib.ASIGetID.argtypes = [c.c_int, c.POINTER(c.c_char)]
+zwolib.ASIGetID.restype = c.c_int
+
+# Include file suggests:
+# zwolib.ASISetID.argtypes = [c.c_int, c.POINTER(c.c_char)]
+#
+# Suspect it should really be
+# zwolib.ASISetID.argtypes = [c.c_int, c.POINTER(c.c_char)]
+#
+# zwolib.ASISetID.restype = c.c_int
+#
+# Leave out support for ASISetID for now
+
+
+zwolib.ASIGetGainOffset.argtypes = [c.c_int,
+                                    c.POINTER(c.c_int),
+                                    c.POINTER(c.c_int),
+                                    c.POINTER(c.c_int),
+                                    c.POINTER(c.c_int)]
+zwolib.ASIGetGainOffset.restype = c.c_int
