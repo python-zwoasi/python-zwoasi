0a226c5293da892b38172ced7759768ea5d238d8
Steve Marple <stevemarple@googlemail.com>
2017-02-01 22:13:25 +0000

    Bump version: 0.0.15 → 0.0.16

--- zwoasi/__init__.py
+++ zwoasi/__init__.py
@@ -11,7 +11,7 @@ import time
 import traceback
 
 __author__ = 'Steve Marple'
-__version__ = '0.0.15'
+__version__ = '0.0.16'
 __license__ = 'MIT'
 
 
--- zwoasi/examples/zwoasi_demo.py
+++ zwoasi/examples/zwoasi_demo.py
@@ -6,7 +6,7 @@ import zwoasi as asi
 
 
 __author__ = 'Steve Marple'
-__version__ = '0.0.15'
+__version__ = '0.0.16'
 __license__ = 'MIT'
 
 
