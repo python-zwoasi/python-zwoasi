a978638a908264740b99c11ea2f50b076b292f36
Steve Marple <stevemarple@googlemail.com>
2017-02-05 23:45:52 +0000

    Mock out numpy etc
    From read the docs FAQ "I get import errors on libraries that depend on
    C modules"
    
    http://docs.readthedocs.io/en/latest/faq.html#i-get-import-errors-on-
    libraries-that-depend-on-c-modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,15 @@
 
 import sys
 import os
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['numpy', 'scipy']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
